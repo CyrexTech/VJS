016debb4d13ba1f1e26ad22f950b6aaaad41fe89
CyrexTech <sunnhore@gmail.com>
2024-11-14 14:51:11 +0100

    push

---
+++ vJS/inputTorsdag/script.js
@@ -0,0 +1,8 @@
+const output = document.getElementById('outputDiv');
+
+function printText(){
+    const input = document.getElementById('textInput').value;
+    output.innerHTML += /*HTML*/`
+    <li><input type="checkbox"> ${input}</li>
+    `;
+    }
